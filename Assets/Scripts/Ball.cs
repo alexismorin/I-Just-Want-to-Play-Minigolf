92b85fab6961e621141b29083853e1cc23e6e064
Alexis <alexis.morin.mail@gmail.com>
2019-03-06 23:52:59 -0500

    ball code

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Transform playerPosition;
+
+    void OnEnable () {
+        Transform playerPosition = GameObject.Find ("Player").transform;
+    }
+
+    void OnCollisionEnter (Collision collision) {
+        if (collision.gameObject.tag == "club") {
+            Invoke ("Adjust", 4f);
+        }
+
+    }
+
+    void Adjust () {
+        playerPosition.position = this.transform.position;
+    }
+}
